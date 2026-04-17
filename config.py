ae56ba4e60bbfd271037cb2f9055f6b15e941e3c
mocktestbot <tmock730@gmail.com>
2026-04-17 13:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ MONGO_URI = os.getenv("MONGO_URI", "")
 ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
 PUBLIC_CHANNEL_ID = os.getenv("PUBLIC_CHANNEL_ID", "")
 COMPLAINT_CHANNEL_ID = os.getenv("COMPLAINT_CHANNEL_ID", "")
-LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "") # प्राइवेट चैनल जहाँ नए यूज़र का डेटा आएगा
+LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "")
 CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/telegram")
 WEBAPP_BASE_URL = os.getenv("WEBAPP_BASE_URL", "https://mocktestbot.github.io/Webapp01/?test_id=")
 
